cf00770b75993757350edb7a26db4575b408fb02
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2026-05-27 09:12:25 -0600

    fix index

--- src/lib/components/ui/input/TextArea.svelte
+++ src/lib/components/ui/input/TextArea.svelte
@@ -105,7 +105,7 @@
 
 	{#if hasErrors}
 		<div class="form-feedback-container" id="{name}-error" role="alert">
-			{#each errorList as err}
+			{#each errorList as err (err)}
 				<span class="form-feedback form-feedback--{status} text-body-small">
 					{err}
 				</span>
