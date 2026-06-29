c145ca4b16cd8ebe090f2c002c676173709bd3f1
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2026-06-29 17:50:02 -0600

    fix dates

--- src/routes/(authenticated)/evaluacion/[evaluacionCode]/etapa/+page.svelte
+++ src/routes/(authenticated)/evaluacion/[evaluacionCode]/etapa/+page.svelte
@@ -4,20 +4,29 @@
 	import { createModalManager } from '$lib/utils/modalManager.svelte';
 	import type { EvaluacionEtapaItem } from '$lib/schemas/evaluacionEtapa.schema';
 	import { page } from '$app/state';
+	import EditarEvaluacionEtapaForm from '$lib/components/evaluacion/etapa/EditarEvaluacionEtapaForm.svelte';
 
 	let evaluacionCode = page.params.evaluacionCode;
-	let evaluacionEtapaItems = getEvaluacionEtapa().filter(item=> item.evaluacion.code === evaluacionCode);
+	let evaluacionEtapaItems = getEvaluacionEtapa().filter(
+		(item) => item.evaluacion.code === evaluacionCode
+	);
 	let modalEtapa = createModalManager<EvaluacionEtapaItem>();
 </script>
 
-	<main>
-		<EvaluacionEtapa
-			gridArea="etapa"
-			showHeader={true}
-			title="Etapas de la evaluacion"
-			items={evaluacionEtapaItems}
-			onClickEditar={modalEtapa.handlers('edit').onClickItem}
+<main>
+	<EvaluacionEtapa
+		gridArea="etapa"
+		showHeader={true}
+		title="Etapas de la evaluacion"
+		items={evaluacionEtapaItems}
+		onClickEditar={modalEtapa.handlers('calendar').onClickItem}
+	/>
+
+	{#if modalEtapa.selectedItem}
+		<EditarEvaluacionEtapaForm
+			open={modalEtapa.isOpen('calendar')}
+			item={modalEtapa.selectedItem}
+			onClose={modalEtapa.close}
 		/>
-	</main>
-
-
+	{/if}
+</main>
