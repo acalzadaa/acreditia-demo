69d4660ec0a60632ff80ba11295a2889c6587705
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2026-07-08 15:34:38 -0600

    feature/confirm modals

---
+++ src/lib/components/ui/confirm/ConfirmDeleteModal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import ConfirmModal from './ConfirmModal.svelte';
+
+	interface Props {
+		demo: boolean;
+		id: string;
+		open: boolean;
+		onClose: () => void;
+	}
+
+	let { open = false, onClose, id, demo = false }: Props = $props();
+
+	function handleClose() {
+		onClose();
+	}
+</script>
+
+<ConfirmModal
+	{demo}
+	{id}
+	message="¿Estas seguro que deseas borrar este elemento?"
+	{open}
+	title="Confirmar borrado"
+	onClose={handleClose}
+	formAction="?/delete"
+	buttonLabel="Borrar"
+	actionButtonVariant="critical"
+/>
---
+++ src/lib/components/ui/confirm/ConfirmRestoreModal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import ConfirmModal from './ConfirmModal.svelte';
+
+	interface Props {
+		demo: boolean;
+		id: string;
+		open: boolean;
+		onClose: () => void;
+	}
+
+	let { open = false, onClose, id, demo = false }: Props = $props();
+
+	function handleClose() {
+		onClose();
+	}
+</script>
+
+<ConfirmModal
+	{demo}
+	{id}
+	message="¿Esta seguro que desea restaurar este elemento?"
+	{open}
+	title="Confirmar restauracion"
+	onClose={handleClose}
+	formAction="?/restore"
+	buttonLabel="Restaurar"
+	actionButtonVariant="primary"
+/>
